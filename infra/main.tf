e7aeac06542d503cfe185d8a1ef5a548f8d4e91f
Ronny Yepez <95048729+rwyepez@users.noreply.github.com>
2024-04-27 08:42:37 -0500

    add apigw

--- infra/main.tf
+++ infra/main.tf
@@ -129,13 +129,13 @@ resource "aws_cloudwatch_log_group" "car_lambda_logs" {
 
 resource "aws_api_gateway_rest_api" "my_api" {
   name        = "CarAPI"
-  description = "API to manage car data"
+  description = "API to manage cars data"
 }
 
 resource "aws_api_gateway_resource" "car_resource" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "car"
+  path_part   = "cars"
 }
 
 resource "aws_api_gateway_method" "car_post_method" {
@@ -145,7 +145,7 @@ resource "aws_api_gateway_method" "car_post_method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_integration" "lambda_post_integration" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.car_resource.id
   http_method = aws_api_gateway_method.car_post_method.http_method
@@ -154,9 +154,26 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   integration_http_method = "POST"
 }
 
+resource "aws_api_gateway_method" "car_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.car_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.car_resource.id
+  http_method = aws_api_gateway_method.car_get_method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.car_lambda_function.invoke_arn
+  integration_http_method = "GET"
+}
+
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda_integration
+    aws_api_gateway_integration.lambda_get_integration,
+    aws_api_gateway_integration.lambda_post_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name  = "dev"
@@ -169,4 +186,3 @@ resource "aws_lambda_permission" "api_gateway_permission" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }
-
