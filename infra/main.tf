af5997105748dc31ee71a740aa24bc08dc3ff9bc
Ronny Yepez <95048729+rwyepez@users.noreply.github.com>
2024-04-27 08:17:43 -0500

    add apigw

--- infra/main.tf
+++ infra/main.tf
@@ -125,3 +125,48 @@ resource "aws_cloudwatch_log_group" "car_lambda_logs" {
   name              = "/aws/lambda/${aws_lambda_function.car_lambda_function.function_name}"
   retention_in_days = 1
 }
+
+
+resource "aws_api_gateway_rest_api" "my_api" {
+  name        = "CarAPI"
+  description = "API to manage car data"
+}
+
+resource "aws_api_gateway_resource" "car_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "car"
+}
+
+resource "aws_api_gateway_method" "car_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.car_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.car_resource.id
+  http_method = aws_api_gateway_method.car_post_method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.car_lambda_function.invoke_arn
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.car_lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
+}
+
