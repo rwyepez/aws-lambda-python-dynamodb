6161787ad37c9d68c834a9797655f138b16b6cc8
Ronny Yepez <95048729+rwyepez@users.noreply.github.com>
2024-05-13 16:54:39 -0500

    update to modules

--- infra/lambdas/outputs.tf
+++ infra/lambdas/outputs.tf
@@ -1,5 +1,5 @@
 output "car_lambda_function_arn" {
-  value = aws_lambda_function.car_lambda_function.arn
+  value = aws_lambda_function.car_lambda_function.invoke_arn
 }
 
 output "car_lambda_function_name" {
