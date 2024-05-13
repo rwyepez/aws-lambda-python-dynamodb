cce020afb63700af2153acaa02850ce870222d3c
Ronny Yepez <95048729+rwyepez@users.noreply.github.com>
2024-05-13 16:43:51 -0500

    update to modules

---
+++ infra/apigw/variables.tf
@@ -0,0 +1,9 @@
+variable "car_lambda_function_arn" {
+  type        = string
+  description = "lambda function arn"
+}
+
+variable "car_lambda_function_name" {
+  type        = string
+  description = "lambda function name"
+}
--- infra/main.tf
+++ infra/main.tf
@@ -15,66 +15,8 @@ module "lambda" {
   policy_query_dynamodb_arn    = module.policies.policy_query_dynamodb_arn
 }
 
-resource "aws_api_gateway_rest_api" "my_api" {
-  name        = "CarAPI"
-  description = "API to manage cars data"
-}
-
-resource "aws_api_gateway_resource" "car_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "cars"
-}
-
-resource "aws_api_gateway_method" "car_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.car_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_post_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.car_resource.id
-  http_method             = aws_api_gateway_method.car_post_method.http_method
-  type                    = "AWS_PROXY"
-  uri                     = module.lambda.car_lambda_function_arn
-  integration_http_method = "POST"
-}
-
-resource "aws_api_gateway_method" "car_get_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.car_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-
-  request_parameters = {
-    "method.request.querystring.carId" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "lambda_get_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.id
-  resource_id             = aws_api_gateway_resource.car_resource.id
-  http_method             = aws_api_gateway_method.car_get_method.http_method
-  type                    = "AWS_PROXY"
-  uri                     = module.lambda.car_lambda_function_arn
-  integration_http_method = "GET"
-}
-
-resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda_get_integration,
-    aws_api_gateway_integration.lambda_post_integration
-  ]
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev"
-}
-
-resource "aws_lambda_permission" "api_gateway_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda.car_lambda_function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
+module "apigw" {
+  source                   = "./apigw"
+  car_lambda_function_arn  = module.lambda.car_lambda_function_arn
+  car_lambda_function_name = module.lambda.car_lambda_function_name
 }
